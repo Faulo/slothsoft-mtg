3c5793dbee0a4910f2afc9afc418621976d0cc70
Faulo <FauloLio@gmail.com>
2018-04-03 14:51:42 +0200

    removed unused

--- assets/unused/data/httpConfig.php
+++
@@ -1,6 +0,0 @@
-<?php
-use Slothsoft\Farah\HTTPResponse;
-
-HTTPResponse::setHttpConfig([
-    'cache-duration' => 0
-]);
--- assets/unused/data/image.php
+++
@@ -1,8 +0,0 @@
-<?php
-$card = [];
-$card['color'] = $this->httpRequest->getInputValue('color');
-
-$path = \Slothsoft\MTG\OracleInfo::getColorPath($card);
-if ($path = realpath($path)) {
-    return \Slothsoft\Farah\HTTPFile::createFromPath($path);
-}

a32928b8ea3a5622ca68bbbf9d02ee2e48d75bbd
Daniel Schulz (Estelle) <faulolio@gmail.com>
2025-10-25 22:04:01 +0200

    fix: tests

--- tests/API/AssetsManifestTest.php
+++ tests/API/AssetsManifestTest.php
@@ -7,6 +7,6 @@ use Slothsoft\FarahTesting\Module\AbstractXmlManifestTest;
 class AssetsManifestTest extends AbstractXmlManifestTest {
     
     protected static function getManifestDirectory(): string {
-        return dirname(__DIR__) . DIRECTORY_SEPARATOR . 'assets';
+        return dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . 'assets';
     }
 }

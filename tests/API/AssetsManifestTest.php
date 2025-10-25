d3c6e9270c20c7eef383883f49ceaeab549e6239
Daniel Schulz (Estelle) <faulolio@gmail.com>
2025-10-25 21:49:34 +0200

    fix: tests

--- tests/API/AssetsManifestTest.php
+++ tests/API/AssetsManifestTest.php
@@ -1,8 +1,8 @@
 <?php
 declare(strict_types = 1);
-namespace Slothsoft\MTG;
+namespace Slothsoft\MTG\API;
 
-use Slothsoft\Farah\ModuleTests\AbstractXmlManifestTest;
+use Slothsoft\FarahTesting\Module\AbstractXmlManifestTest;
 
 class AssetsManifestTest extends AbstractXmlManifestTest {
     

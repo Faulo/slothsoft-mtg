9aa57f7549491be3e0e01f324d19c0882e90a330
Daniel Schulz (Madeline) <faulolio@gmail.com>
2026-01-17 01:24:24 +0100

    build: update files

---
+++ tests/Oracle/Work/AbstractOracleWorkTest.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types = 1);
+namespace Slothsoft\MTG\Oracle\Work;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * AbstractOracleWorkTest
+ *
+ * @see AbstractOracleWork
+ *
+ * @todo auto-generated
+ */
+class AbstractOracleWorkTest extends TestCase {
+    
+    public function testClassExists(): void {
+        $this->assertTrue(class_exists(AbstractOracleWork::class), "Failed to load class 'Slothsoft\MTG\Oracle\Work\AbstractOracleWork'!");
+    }
+}
---
+++ tests/Oracle/Work/EndTest.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types = 1);
+namespace Slothsoft\MTG\Oracle\Work;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * EndTest
+ *
+ * @see End
+ *
+ * @todo auto-generated
+ */
+class EndTest extends TestCase {
+    
+    public function testClassExists(): void {
+        $this->assertTrue(class_exists(End::class), "Failed to load class 'Slothsoft\MTG\Oracle\Work\End'!");
+    }
+}
